8886087ace572cbf39a54d5a0ec3391a3c550779
abdurrahim83123-glitch <abdurrahim83123@gmail.com>
2026-02-06 00:03:09 +0530

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,4 +1,14 @@
-
 <?php
-$conn = new mysqli("localhost","root","root","service_booking", 8888);
-?>
+$conn = new mysqli(
+    getenv("MYSQLHOST"),
+    getenv("MYSQLUSER"),
+    getenv("MYSQLPASSWORD"),
+    getenv("MYSQLDATABASE"),
+    getenv("MYSQLPORT")
+);
+
+if ($conn->connect_error) {
+    die("Database Connection Failed");
+}
+
+$conn->set_charset("utf8mb4");
